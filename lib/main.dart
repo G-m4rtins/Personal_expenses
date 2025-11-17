0e98e8bdafe87b6e5394579c58fffbe75211367b
G-m4rtins <th3gabrielmartins@gmail.com>
2025-11-16 23:59:41 -0300

    add intl package and format transaction value and date display

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 
 import 'models/transaction.dart';
 
@@ -70,7 +71,7 @@ class MyHomePage extends StatelessWidget {
                       ),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                          tr.value.toString(),
+                          'R\$  ${tr.value.toStringAsFixed(2)}',
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
@@ -89,7 +90,7 @@ class MyHomePage extends StatelessWidget {
                             )
                         ),
                         Text(
-                          tr.date.toString(),
+                          DateFormat('d MMM y').format(tr.date),
                             style: TextStyle(
                               color: Colors.grey[600],
                             )
